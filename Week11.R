1a56b739099c9fc5fc5d36f8df29a82bb52e7e86
denzbenga <collinsdaniel1413@gmail.com>
2026-03-13 14:50:46 +0100

    website changes

--- Week11.R
+++ Week11.R
@@ -9,3 +9,13 @@ ten_bands_nvlty_tempo_2 |>
   xlim(40, 176) +                         # Adjust the limits to the desired time range
   theme_minimal() +
   labs(x = "Time (s)", y = "Novelty")
+
+
+on_sight_act |> 
+  pivot_longer(-TIME, names_to = "tempo") |> 
+  mutate(tempo = as.numeric(tempo)) |> 
+  ggplot(aes(x = TIME, y = tempo, fill = value)) +
+  geom_raster() +
+  scale_fill_viridis_c(guide = "none") +
+  labs(x = "Time (s)", y = "Tempo (BPM)") +
+  theme_classic()
